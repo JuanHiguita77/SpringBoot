f41f35f4c88aaa9875cdd16a313f423432963124
Juan Yepes <juanferhiguita65@gmail.com>
2024-12-03 17:27:47 -0500

    Add: New full stack Angular +  graphql Project

---
+++ GraphQLCrud/src/main/java/com/juan/GraphQLCrud/GraphQlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.juan.GraphQLCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlCrudApplication.class, args);
+	}
+
+}
---
+++ GraphQLCrud/src/test/java/com/juan/GraphQLCrud/GraphQlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.GraphQLCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
